925a90b2603c11f0f1ad5b3688cbf1ad52bc161d
hichenoe <44032477+hichenoe@users.noreply.github.com>
2018-11-13 12:32:26 -0500

    Add files via upload

---
+++ final_project.vhd
@@ -0,0 +1,226 @@
+--------------------------------------------------------------------------------
+--
+--   FileName:         ps2_keyboard.vhd
+--   Dependencies:     debounce.vhd
+--   Design Software:  Quartus II 32-bit Version 12.1 Build 177 SJ Full Version
+--
+--   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
+--   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+--   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
+--   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
+--   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
+--   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+--   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+--   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+--
+--   Version History
+--   Version 1.0 11/25/2013 Scott Larson
+--     Initial Public Release
+--    
+--------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_arith.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY final_project IS
+  GENERIC(
+    clk_freq              : INTEGER := 50_000_000; --system clock frequency in Hz
+    debounce_counter_size : INTEGER := 8);         --set such that (2^size)/clk_freq = 5us (size = 8 for 50MHz)
+  PORT(
+    clk          : IN  STD_LOGIC;                     --system clock
+    ps2_clk      : IN  STD_LOGIC;                     --clock signal from PS/2 keyboard
+    ps2_data     : IN  STD_LOGIC;                     --data signal from PS/2 keyboard
+	 led1			  : OUT STD_LOGIC;
+	 led2			  : OUT STD_LOGIC;
+	 led3			  : OUT STD_LOGIC;
+	 led4			  : OUT STD_LOGIC;
+	 led5			  : OUT STD_LOGIC;
+	 led6			  : OUT STD_LOGIC;
+	 led7			  : OUT STD_LOGIC;
+	 led8			  : OUT STD_LOGIC;
+	 led9			  : OUT STD_LOGIC;
+	 led10		  : OUT STD_LOGIC;
+	 led11		  : OUT STD_LOGIC;
+	 led12		  : OUT STD_LOGIC;
+	 led13		  : OUT STD_LOGIC;
+    ps2_code_new : OUT STD_LOGIC;                     --flag that new PS/2 code is available on ps2_code bus
+    ps2_code     : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)); --code received from PS/2
+END final_project;
+
+ARCHITECTURE logic OF final_project IS
+  SIGNAL sync_ffs     : STD_LOGIC_VECTOR(1 DOWNTO 0);       --synchronizer flip-flops for PS/2 signals
+  SIGNAL ps2_clk_int  : STD_LOGIC;                          --debounced clock signal from PS/2 keyboard
+  SIGNAL ps2_data_int : STD_LOGIC;                          --debounced data signal from PS/2 keyboard
+  SIGNAL ps2_word     : STD_LOGIC_VECTOR(10 DOWNTO 0);      --stores the ps2 data word
+  SIGNAL error        : STD_LOGIC;                          --validate parity, start, and stop bits
+  SIGNAL count_idle   : INTEGER RANGE 0 TO clk_freq/18_000; --counter to determine PS/2 is idle
+  SIGNAL keyval       : STD_LOGIC_VECTOR(8 DOWNTO 1);
+  
+  --declare debounce component for debouncing PS2 input signals
+  COMPONENT debounce IS
+    GENERIC(
+      counter_size : INTEGER); --debounce period (in seconds) = 2^counter_size/(clk freq in Hz)
+    PORT(
+      clk    : IN  STD_LOGIC;  --input clock
+      button : IN  STD_LOGIC;  --input signal to be debounced
+      result : OUT STD_LOGIC); --debounced signal
+  END COMPONENT;
+BEGIN
+
+  --synchronizer flip-flops
+  PROCESS(clk)
+  BEGIN
+    IF(clk'EVENT AND clk = '1') THEN  --rising edge of system clock
+      sync_ffs(0) <= ps2_clk;           --synchronize PS/2 clock signal
+      sync_ffs(1) <= ps2_data;          --synchronize PS/2 data signal
+    END IF;
+  END PROCESS;
+
+  --debounce PS2 input signals
+  debounce_ps2_clk: debounce
+    GENERIC MAP(counter_size => debounce_counter_size)
+    PORT MAP(clk => clk, button => sync_ffs(0), result => ps2_clk_int);
+  debounce_ps2_data: debounce
+    GENERIC MAP(counter_size => debounce_counter_size)
+    PORT MAP(clk => clk, button => sync_ffs(1), result => ps2_data_int);
+
+  --input PS2 data
+  PROCESS(ps2_clk_int)
+  BEGIN
+    IF(ps2_clk_int'EVENT AND ps2_clk_int = '0') THEN    --falling edge of PS2 clock
+      ps2_word <= ps2_data_int & ps2_word(10 DOWNTO 1);   --shift in PS2 data bit
+    END IF;
+  END PROCESS;
+    
+  --verify that parity, start, and stop bits are all correct
+  error <= NOT (NOT ps2_word(0) AND ps2_word(10) AND (ps2_word(9) XOR ps2_word(8) XOR
+        ps2_word(7) XOR ps2_word(6) XOR ps2_word(5) XOR ps2_word(4) XOR ps2_word(3) XOR 
+        ps2_word(2) XOR ps2_word(1)));  
+
+  --determine if PS2 port is idle (i.e. last transaction is finished) and output result
+  PROCESS(clk)
+  BEGIN
+    IF(clk'EVENT AND clk = '1') THEN           --rising edge of system clock
+    
+      IF(ps2_clk_int = '0') THEN                 --low PS2 clock, PS/2 is active
+        count_idle <= 0;                           --reset idle counter
+      ELSIF(count_idle /= clk_freq/18_000) THEN  --PS2 clock has been high less than a half clock period (<55us)
+          count_idle <= count_idle + 1;            --continue counting
+      END IF;
+      
+      IF(count_idle = clk_freq/18_000 AND error = '0') THEN  --idle threshold reached and no errors detected
+        ps2_code_new <= '1';                                   --set flag that new PS/2 code is available
+        ps2_code <= ps2_word(8 DOWNTO 1);                      --output new PS/2 code
+		  keyval <= ps2_word(8 DOWNTO 1);
+		 IF keyval = X"1C" THEN
+				led1 <= '1';
+		 ELSIF keyval = X"1D" then
+				 led2 <= '1';
+		 ELSIF keyval = X"1B" then
+				 led3 <= '1';
+		 ELSIF keyval = X"24" then
+				 led4 <= '1';
+		 ELSIF keyval = X"23" then
+				 led5 <= '1';
+		 ELSIF keyval = X"2B" then
+				 led6 <= '1';
+		 ELSIF keyval = X"2C" then
+				 led7 <= '1';
+		 ELSIF keyval = X"34" then
+				 led8 <= '1';
+		 ELSIF keyval = X"35" then
+				 led9 <= '1';
+		 ELSIF keyval = X"33" then
+				 led10 <= '1';
+		 ELSIF keyval = X"3C" then
+				 led11 <= '1';
+		 ELSIF keyval = X"3B" then
+				 led12 <= '1';
+		 ELSIF keyval = X"42" then
+				 led13 <= '1';		 
+		 ELSE 
+				led1 <= '0';
+				led2 <= '0';
+				led3 <= '0';
+				led4 <= '0';
+				led5 <= '0';
+				led6 <= '0';
+				led7 <= '0';
+				led8 <= '0';
+				led9 <= '0';
+				led10 <= '0';
+				led11 <= '0';
+				led12 <= '0';
+				led13 <= '0';
+		  END IF;
+      ELSE                                                   --PS/2 port active or error detected
+        ps2_code_new <= '0';                                   --set flag that PS/2 transaction is in progress
+      END IF;
+      
+    END IF;
+  END PROCESS;
+  
+END logic;
+
+
+--------------------------------------------------------------------------------
+--
+--   FileName:         debounce.vhd
+--   Dependencies:     none
+--   Design Software:  Quartus II 32-bit Version 11.1 Build 173 SJ Full Version
+--
+--   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
+--   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+--   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
+--   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
+--   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
+--   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+--   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+--   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+--
+--   Version History
+--   Version 1.0 3/26/2012 Scott Larson
+--     Initial Public Release
+--
+--------------------------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY debounce IS
+  GENERIC(
+    counter_size  :  INTEGER := 19); --counter size (19 bits gives 10.5ms with 50MHz clock)
+  PORT(
+    clk     : IN  STD_LOGIC;  --input clock
+    button  : IN  STD_LOGIC;  --input signal to be debounced
+    result  : OUT STD_LOGIC); --debounced signal
+END debounce;
+
+ARCHITECTURE logic OF debounce IS
+  SIGNAL flipflops   : STD_LOGIC_VECTOR(1 DOWNTO 0); --input flip flops
+  SIGNAL counter_set : STD_LOGIC;                    --sync reset to zero
+  SIGNAL counter_out : STD_LOGIC_VECTOR(counter_size DOWNTO 0) := (OTHERS => '0'); --counter output
+BEGIN
+
+  counter_set <= flipflops(0) xor flipflops(1);   --determine when to start/reset counter
+  
+  PROCESS(clk)
+  BEGIN
+    IF(clk'EVENT and clk = '1') THEN
+      flipflops(0) <= button;
+      flipflops(1) <= flipflops(0);
+      If(counter_set = '1') THEN                  --reset counter because input is changing
+        counter_out <= (OTHERS => '0');
+      ELSIF(counter_out(counter_size) = '0') THEN --stable input time is not yet met
+        counter_out <= counter_out + 1;
+      ELSE                                        --stable input time is met
+        result <= flipflops(1);
+      END IF;    
+    END IF;
+  END PROCESS;
+END logic;
